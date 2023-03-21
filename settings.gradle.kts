502f9e7cc5a0cfe9cd0c36771d2c4e28fd5924bd
Martijn Muijsers <martijnmuijsers@live.nl>
2023-03-21 20:20:16 +0100

    Update Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Locale
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -5,6 +7,34 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+if (!file(".git").exists()) {
+    val errorText = """
+        
+        =====================[ ERROR ]=====================
+         The Paper project directory is not a properly cloned Git repository.
+         
+         In order to build Paper from source you must clone
+         the Paper repository using Git, not download a code
+         zip from GitHub.
+         
+         Built Paper jars are available for download at
+         https://papermc.io/downloads
+         
+         See https://github.com/PaperMC/Paper/blob/master/CONTRIBUTING.md
+         for further information on building and modifying Paper.
+        ===================================================
+    """.trimIndent()
+    error(errorText)
+}
+
 rootProject.name = "gale" // Gale - build changes
 
-include("gale-api", "gale-server") // Gale - build changes
+for (name in listOf("Gale-API", "Gale-Server")) { // Gale - build changes
+    val projName = name.toLowerCase(Locale.ENGLISH)
+    include(projName)
+    findProject(":$projName")!!.projectDir = file(name)
+}
