798ae738ad772e581f33ff02cbb3a9d167f68fd4
Martijn Muijsers <martijnmuijsers@live.nl>
2023-04-29 15:31:21 +0200

    Updated Upstream (Paper)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.4.0"
+    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.2"
     id("com.github.ManifestClasspath") version "0.1.0-RELEASE"
 }
 
