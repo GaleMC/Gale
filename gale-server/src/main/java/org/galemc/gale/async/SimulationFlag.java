626c3d1135d67b306a2a9f847762d4def0aef2f3
Martijn Muijsers <martijnmuijsers@live.nl>
2026-06-23 20:56:47 +0200

    Create skeleton

---
+++ gale-server/src/main/java/org/galemc/gale/async/SimulationFlag.java
@@ -0,0 +1,14 @@
+package org.galemc.gale.async;
+
+public final class SimulationFlag {
+
+    private SimulationFlag() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * False while simulating.
+     */
+    public static boolean REAL = true;
+
+}
